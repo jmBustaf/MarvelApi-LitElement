77f249030213d48c74cc3ae0e58674b4ca7f7393
Jose Miguel <josemiguelbf8@gmail.com>
2022-08-16 10:12:10 -0500

    create project apirest-marvel with LitElement

---
+++ apirest-marvel.js
@@ -0,0 +1,3 @@
+import { ApirestMarvel } from './src/ApirestMarvel.js';
+
+window.customElements.define('apirest-marvel', ApirestMarvel);
---
+++ index.js
@@ -0,0 +1,1 @@
+export { ApirestMarvel } from './src/ApirestMarvel.js';
